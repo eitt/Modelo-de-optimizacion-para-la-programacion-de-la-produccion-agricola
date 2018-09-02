cd2f72936ccc047532f3fb726cd38a6488bf954d
taleroleo_casa <leonardo.talero@gmail.com>
2018-09-02 10:02:47 -0500

    Creación de archivos
    son creados los documentos de precios (a manera de ejemplo) y el documento base para generar el modelo de optimización.

---
+++ Base.m
@@ -0,0 +1,7 @@
+%Este es el código para solucionar de manera exacta un modelo de
+%optimización multiobjetivo (como aproximación se trabajan las dos
+%funciones de manera independiente), el cual hace parte del proyecto de
+%investigación titulado: Modelo de optimización multiobjetivo para la 
+% programación de la producción agrícola a pequeña escala en Santander,
+% Colombia, desarrollado por Leonardo Talero, bajo la dirección de Henry
+% Lamos & Edwin Garavito
